3758e556fbbc8f6fe4652886f6219ccee752aa39
lkusam <lkusam@kent.edu>
2022-08-29 11:07:47 -0400

    -- Code for checking the results -- Added sample to check for other results

--- index.js
+++ index.js
@@ -19,17 +19,45 @@
  * will pass valid data to your function.
  */
 
-function checkForBingo (bingoCard, drawnNumbers) {
+function checkForBingo(bingoCard, drawnNumbers) {
   // this code for debug purposes, you can remove.
-  console.log('Drawn Numbers: ' + JSON.stringify(drawnNumbers));
-
-  for (let i=0, len=bingoCard.length; i<len; i++) {
-    let row = Math.floor(i/5);
+  console.log("Drawn Numbers: " + JSON.stringify(drawnNumbers));
+  let selectedCells = [];
+  let selectedIndexes = [];
+  console.log(drawnNumbers.length);
+  for (let i = 0, len = bingoCard.length; i < len; i++) {
+    let row = Math.floor(i / 5);
     let col = i % 5;
-   //  console.log(`${row},${col}: ${bingoCard[i]}`);
+    // considering that we get more than 5 if the there is no play for Free Item
+    let includeFree = drawnNumbers.length >= 5 ? false : true;
+    if (drawnNumbers.includes(bingoCard[i]) || (includeFree && bingoCard[i] == "FREE")) {
+      selectedCells.push({ x: row, y: col });
+      selectedIndexes.push(i);
+      console.log(`${row},${col}: ${bingoCard[i]}`);
+    }
   }
+  console.log(selectedCells);
+  console.log(selectedIndexes);
 
-  return false;
+  let res = checkWin();
+  console.log("Is a winner : " + res);
+
+  return res;
+
+  function checkWin() {
+    if (selectedIndexes.length > 0) {
+      let previousDiff;
+      let currentDiff;
+      for (let index = 0; index < selectedIndexes.length - 2; index++) {
+        const element = selectedIndexes[index];
+        currentDiff = selectedIndexes[index + 1] - selectedIndexes[index];
+        if (index != 0 && previousDiff != currentDiff) {
+          return false;
+        } else previousDiff = currentDiff;
+      }
+      return true;
+    }
+  }
 }
 
 module.exports = checkForBingo;
@@ -38,28 +66,24 @@ module.exports = checkForBingo;
 
 // this should return true with diagonal + free
 checkForBingo(
-  [
-    8, 29, 35, 54, 65,
-    13, 24, 44, 48, 67,
-    9, 21, 'FREE', 59, 63,
-    7, 19, 34, 53, 61,
-    1, 20, 33, 46, 72
-  ],
-  [
-    8, 24, 53, 72
-  ]
+  [8, 29, 35, 54, 65, 13, 24, 44, 48, 67, 9, 21, "FREE", 59, 63, 7, 19, 34, 53, 61, 1, 20, 33, 46, 72],
+  [8, 24, 53, 72]
+);
+
+// Diagonal other way
+checkForBingo(
+  [32, 29, 35, 54, 8, 13, 66, 44, 24, 67, 9, 21, "FREE", 59, 63, 7, 53, 34, 19, 61, 72, 20, 33, 46, 1],
+  [8, 24, 53, 72]
+);
+
+// column
+checkForBingo(
+  [32, 29, 35, 54, 8, 13, 66, 44, 24, 67, 9, 21, "FREE", 59, 63, 7, 53, 34, 19, 61, 72, 20, 33, 46, 1],
+  [32, 13, 9, 7, 72]
 );
 
 // this should return false
 checkForBingo(
-  [
-   8, 29, 35, 54, 65,
-   13, 24, 44, 48, 67,
-   9, 21, 'FREE', 59, 63,
-   7, 19, 34, 53, 61,
-   1, 20, 33, 46, 72
-  ],
-  [
-    1, 33, 53, 65, 29, 75
-  ]
+  [8, 29, 35, 54, 65, 13, 24, 44, 48, 67, 9, 21, "FREE", 59, 63, 7, 19, 34, 53, 61, 1, 20, 33, 46, 72],
+  [1, 33, 53, 65, 29, 75]
 );
